3dfcefd7554bab2375f60cb1f0f24a3d41e86ecf
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-09-08 10:04:14 +0200

    small refactoring to RpcConsumer

--- src/simplerpcpy/rpc_consumer.py
+++ src/simplerpcpy/rpc_consumer.py
@@ -3,25 +3,13 @@ import json
 from simplerpcpy.messaging import Client
 
 
+# class _Instrument
 class RpcConsumer:
 
     def __init__(self, topic: str, client: Client, proxy):
         self.topic = topic
         self.client = client
-        filt1 = (name for name in dir(proxy))
-        functions = (getattr(proxy, name) for name in filt1
-                     if not (name.startswith('__') and name.endswith('__')))
-        for func in functions:
-            name = func.__name__
-
-            def scope(name):
-                def newfunc(*args, **kwargs):
-                    self._handle(name, *args, **kwargs)
-
-                newfunc.__name__ = name
-                return newfunc
-
-            setattr(proxy, name, scope(name))
+        redirect_all_calls(proxy, self._handle)
 
     def _handle(self, name, *args, **kwargs):
         o = {'method': name, 'args': args}
@@ -29,3 +17,21 @@ class RpcConsumer:
             o['kwargs'] = kwargs
         payload = json.dumps(o)
         self.client.publish(self.topic, payload)
+
+
+def redirect_all_calls(proxy, handle):
+    filt1 = (name for name in dir(proxy))
+    functions = (f for f in (getattr(proxy, name) for name in filt1
+                             if not (name.startswith('__') and name.endswith('__')))
+                 if callable(f))
+    for func in functions:
+        name = func.__name__
+
+        def scope(name):
+            def newfunc(*args, **kwargs):
+                handle(name, *args, **kwargs)
+
+            newfunc.__name__ = name
+            return newfunc
+
+        setattr(proxy, name, scope(name))
