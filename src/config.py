e727dd388fc68726e0654e294ad50f6b11d500d0
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-07-01 11:47:57 +0200

    disabled ssl because it's more reliable

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ bc.manager_queue = 'example/addition1.0'
 bc.hostname = 'mqtt.flespi.io'
 bc.username = '6XidtbbkABE4iz07XCwRuBZPnlmQtLNqxTrOTsYjSrb0QZVDrfiKG6pShzyzbbNw'  # this is a flespi token
 bc.password = None
-bc.ssl = True
+bc.ssl = False # the ssl was giving some connection unstability
 bc.port = 8883 if bc.ssl else 1883
