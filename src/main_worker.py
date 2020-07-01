b04e12b320f12d845f90224684a84adac3f9b3ae
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-07-01 11:44:23 +0200

    removed root main

--- src/main_worker.py
+++ src/main_worker.py
@@ -2,19 +2,13 @@ from config import distrib_conf
 from simplerpcpy.gmqtt_client import GmqttClient
 from simplerpcpy.job_worker import Worker
 
-
-def main():
-    worker = Worker(distrib_conf, GmqttClient(distrib_conf.broker_config).connect())
-    print(f'{worker.call_sign} is ready')
-    while True:
-        job = worker.get_job()
-        if job:
-            a = job.job['a']
-            b = job.job['b']
-            job.result = a + b
-            print(f'{worker.call_sign} did {a}+{b}={job.result}')
-            worker.job_done(job.job_id)
-
-
-if __name__ == '__main__':
-    main()
+worker = Worker(distrib_conf, GmqttClient(distrib_conf.broker_config).connect())
+print(f'{worker.call_sign} is ready')
+while True:
+    job = worker.get_job()
+    if job:
+        a = job.job['a']
+        b = job.job['b']
+        job.result = a + b
+        print(f'{worker.call_sign} did {a}+{b}={job.result}')
+        worker.job_done(job.job_id)
